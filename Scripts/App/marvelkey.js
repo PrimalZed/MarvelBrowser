624155462670ffda5317974ad3e47d0764942dd4
Primal Zed <primal.zed@gmail.com>
2015-04-05 20:19:39 -0500

    add notes on setting up config for ignoreline

--- Scripts/App/marvelkey.js
+++ Scripts/App/marvelkey.js
@@ -1,3 +1,7 @@
 // define marvelPrivateKey
+// add to git config:
+// $git config --global --add filter.gitignoreline.clean "sed '/\\/\\/gitignore$/'d"
+// $git config --global --add filter.gitignoreline.smudge cat
+
 // End line with '//gitignore' to keep from committing to git
 
